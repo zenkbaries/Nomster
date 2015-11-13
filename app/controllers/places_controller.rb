a8824897012ff80941c35359c6fba68ed064c9dc
John Towery <kbaries@gmail.com>
2015-11-13 18:31:22 +0000

    L22 Edit a place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,6 +20,11 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 	end
 
+	def edit
+		@place = Place.find(params[:id])
+		
+	end
+
 	private
 
 	def place_params
