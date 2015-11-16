a72e51f968d81468ad63f7675ef6278d39205f6c
John Towery <kbaries@gmail.com>
2015-11-16 05:38:52 +0000

    L35 Rating from text to drop down

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :place
+
+	RATINGS = {
+		'one star' 		=> '1_star',
+		'two stars' 	=> '2_star',
+		'three stars' 	=> '3_star',
+		'four stars' 	=> '4_star',
+		'five stars' 	=> '5_star'
+	}
+
+	def humanized_rating
+		RATINGS.invert[self.rating]
+	end
 end
