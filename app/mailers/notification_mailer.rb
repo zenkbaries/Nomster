8a5f095ff25ea7ecfd492d26ebc280038a9ac254
John Towery <kbaries@gmail.com>
2015-11-19 21:10:42 +0000

    L41 Successful Previewing email to my gmail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-  	mail(to:"ken@thefirehoseproject.com",
+  	mail(to:"kbaries@gmail.com",
   		subject: "A comment has been added to your place")
   end
 end
